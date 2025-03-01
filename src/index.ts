db900ab011d2e50e16fd3e32d49f20679d1a4003
Stuart Herbert <stuart@stuartherbert.com>
2025-03-01 16:34:59 +0000

    New: initial unit tests for mustBeCssHsvChannelsData(), from Claude AI

--- src/index.ts
+++ src/index.ts
@@ -119,6 +119,7 @@ export { validateCssHslColorChannelsData } from "./CssHslColor/validateCssHslCol
 export { validateCssHslColorData } from "./CssHslColor/validateCssHslColorData";
 
 export type { CssHsvColorChannelsData } from "./CssHsvColor/CssHsvColorChannelsData.type";
+export { mustBeCssHsvColorChannelsData } from "./CssHsvColor/mustBeCssHsvColorChannelsData";
 export { validateCssHsvColorChannelsData } from "./CssHsvColor/validateCssHsvColorChannelsData";
 
 export { convertConversionModelToHwbChannelsData } from "./CssHwbColor/convertConversionModelToHwbChannelsData";
